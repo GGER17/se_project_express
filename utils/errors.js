aefd269c59046b3b6e23002deea113a1734e9079
Grecia Espinoza <g3901@icloud.com>
2026-02-06 01:23:59 -0600

    utils test 2

--- utils/errors.js
+++ utils/errors.js
@@ -1,1 +1,39 @@
-console.log("utils/errors.js loaded");
+// utils/errors.js
+// Minimal error utilities used by typical tests
+
+class AppError extends Error {
+  constructor(message, status = 500, details = null) {
+    super(message);
+    this.name = this.constructor.name;
+    this.status = status;
+    this.details = details;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class BadRequestError extends AppError {
+  constructor(message = "Bad Request", details = null) {
+    super(message, 400, details);
+  }
+}
+
+class NotFoundError extends AppError {
+  constructor(message = "Not Found", details = null) {
+    super(message, 404, details);
+  }
+}
+
+function toHttpError(err) {
+  if (err instanceof AppError) return err;
+  return new AppError(
+    err.message || "Internal Server Error",
+    err.status || 500
+  );
+}
+
+module.exports = {
+  AppError,
+  BadRequestError,
+  NotFoundError,
+  toHttpError,
+};
